3ab1656b2a8f372c0f5bf39a233f8b9df912afe5
Herb Stahl <ghstahl@gmail.com>
2019-09-17 15:34:56 -0700

    added rudimentary reflection

--- src/GrpcGreeter/Startup.cs
+++ src/GrpcGreeter/Startup.cs
@@ -28,6 +28,7 @@ namespace GrpcGreeter
         {
             services.AddCorrelationId();
             services.AddGrpc();
+            services.AddGrpcReflection();
             services.AddCorrelationEnricher();
         }
 
@@ -64,6 +65,7 @@ namespace GrpcGreeter
                 // Communication with gRPC endpoints must be made through a gRPC client.
                 // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcReflectionService();
             });
             logger.LogInformation("Ready to go!");
         }
--- src/GrpcGreeterClient/Program.cs
+++ src/GrpcGreeterClient/Program.cs
@@ -1,8 +1,11 @@
 ﻿using Grpc.Core;
 using Grpc.Net.Client;
+using Grpc.Reflection.V1Alpha;
 using GrpcGreeter;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using static Grpc.Reflection.V1Alpha.ServerReflection;
 
 namespace GrpcGreeterClient
 {
@@ -13,6 +16,7 @@ namespace GrpcGreeterClient
             GrpcGreeter.HelloReply reply = null;
             // This switch must be set before creating the GrpcChannel/HttpClient.
             AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+          
 
             var insecureChannel = GrpcChannel.ForAddress("http://localhost:4700", new GrpcChannelOptions
             {
@@ -22,8 +26,17 @@ namespace GrpcGreeterClient
                     BaseAddress = new Uri("http://localhost:4700")
                 }
             });
+            var serverReflectionClient = new ServerReflectionClient(insecureChannel);
+            var response = await SingleRequestAsync(serverReflectionClient, new ServerReflectionRequest
+            {
+                ListServices = "" // Get all services
+            });
+            Console.WriteLine("Services:");
+            foreach (var item in response.ListServicesResponse.Service)
+            {
+                Console.WriteLine("- " + item.Name);
+            }
 
-           
             var insecureclient = new Greeter.GreeterClient(insecureChannel);
             try
             {
@@ -53,6 +66,16 @@ namespace GrpcGreeterClient
           
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+        private static async Task<ServerReflectionResponse> SingleRequestAsync(ServerReflectionClient client, ServerReflectionRequest request)
+        {
+            var call = client.ServerReflectionInfo();
+            await call.RequestStream.WriteAsync(request);
+            Debug.Assert(await call.ResponseStream.MoveNext());
+
+            var response = call.ResponseStream.Current;
+            await call.RequestStream.CompleteAsync();
+            return response;
         }
     }
 }
